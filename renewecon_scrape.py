b5354d2a8ed95590cf4b35169f4795318b5d0b60
bengbmccoy <benjamingbmccoy@gmail.com>
2019-01-04 21:12:00 +1100

    This is the first piece of cose to be written for my RenewEconomy scraping and databasing project.
     At this stage the script is at the hello world stage.

---
+++ renewecon_scrape.py
@@ -0,0 +1,34 @@
+'''
+Author: Ben McCoy
+Date started: 2019-01-04
+
+This is my first github project.
+I will create a webscraper that can access RenewEconomy once a day and search
+for articles. Any new articles will be scraped, stored locally, read, analysed
+and ranked by myslef.
+
+The idea is to practice using github, and perhaps build a database of articles
+to use statistical methods and other fun tools on.
+
+This script will be the scraper of this project (see readme.md), this will be
+scheuled to run daily, or so. This script will do the following:
+
+- Open existing article database record
+- Access the RenewEconomy homepage
+- Scrape the page
+- Search and store for artile links
+- Cross reference searching for new articles
+- Go through list of new articls and open web page:
+- Scarpe the page
+- Parse the article text
+- Save the article text and meta data
+
+'''
+
+def main():
+
+    #print 'hello world'
+
+    
+
+main()
